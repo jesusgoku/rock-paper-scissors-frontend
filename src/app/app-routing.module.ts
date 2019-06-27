919ba4b44f319ecd29e391f032ba58a9b9c589d9
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-06-26 20:17:36 -0400

    feat: added app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { GameComponent } from './game/game.component';
+import { RoundComponent } from './round/round.component';
+
+const routes: Routes = [
+  { path: ':id', component: RoundComponent },
+  { path: '', component: GameComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
