a7cd4984b289736ecc66369cd2fc52d8c257b32f
Jesús Urrutia <jesus.urrutia@gmail.com>
2019-06-26 20:18:33 -0400

    feat: enable new modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { RoundComponent } from './round/round.component';
+import { LoadingComponent } from './loading/loading.component';
+import { RoundListComponent } from './round-list/round-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    RoundComponent,
+    LoadingComponent,
+    RoundListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
